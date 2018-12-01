cc97919d47bc2b8a8a0ab4c1c797ca2a39fff782
lt486 <lt486@drexel.edu>
2018-12-01 10:04:00 -0500

    the app file again:

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 CORS(app)
 
 # global variable to save our access_token
-access = None
+access = {}
 
 client = smartcar.AuthClient(
     client_id=CLIENT_ID,
@@ -36,44 +36,48 @@ def exchange():
     global access
     # in a production app you'll want to store this in some kind of
     # persistent storage
-    access = client.exchange_code(code)
+
+    user_access = client.exchange_code(code)
+    user_id = smartcar.get_user_id(user_access["access_token"])
+    access[user_id] = user_access
+
     return '', 200
 
+@app.route('/vehicles', methods=['GET'])
+def vehicles():
+    global access
 
-@app.route('/vehicle', methods=['GET'])
-def vehicle():
+    result = ""
+    for user_id in access:
+        result += f"user: {user_id}\n"
+        token = access[user_id]["access_token"]
+        vehicle_ids = smartcar.get_vehicle_ids(
+            token)['vehicles']
+    
+        for vehicle in [smartcar.Vehicle(id, token) for id in vehicle_ids]:
+            result += f"{vehicle.info()}\n"
+    return result
+
+def detect_accidents():
     # access our global variable to retrieve our access tokens
     global access
-    # the list of vehicle ids
-    vehicle_ids = smartcar.get_vehicle_ids(
-        access['access_token'])['vehicles']
 
-    # instantiate the first vehicle in the vehicle id list
-    vehicle = smartcar.Vehicle(vehicle_ids[0], access['access_token'])
-
-    info = vehicle.info()
-    print(info)
-
-    return jsonify(info)
-def accident():
-      # access our global variable to retrieve our access tokens
-    global access
-    # the list of vehicle ids
-    vehicle_ids = smartcar.get_vehicle_ids(access['access_token'])['vehicles']
-
-    # instantiate the first vehicle in the vehicle id list
-    vehicle = smartcar.Vehicle(vehicle_ids[0], access['access_token'])
-    
-    response_1= vehicle.odometer()
-    time.sleep(5)
-    response_2= vehicle.odometer()
-    time.sleep(5)
-    response_3= vehicle.odometer()
-    v1=(response_2-response_1)/0.05
-    v2=(response_3-response_2)/0.05
-    if(v2-v1>50)
-        #I want to send the client a message to se if he is okay
-    
+    while True:
+        for user_id in access:
+            token = access[user_id]["access_token"]
+            vehicle_ids = smartcar.get_vehicle_ids(
+                token)['vehicles']
+            for vehicle in [smartcar.Vehicle(id, token) for id in vehicle_ids]:
+                response_1= vehicle.odometer()
+                time.sleep(5)
+                response_2= vehicle.odometer()
+                time.sleep(5)
+                response_3= vehicle.odometer()
+                v1=(response_2-response_1)/0.05
+                v2=(response_3-response_2)/0.05
+                if(v2-v1>50):
+                    pass #I want to send the client a message to se if he is okay
 
 if __name__ == '__main__':
     app.run(port=8000)
+    detect_accidents()

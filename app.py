431c1f75dfd219685eb620fbdd5440b634b2b73c
Anton-Cao <cao.anton@gmail.com>
2018-12-01 21:20:08 -0500

    initial db setup

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import time
 import datetime
+import json
+import requests
 from threading import Thread
 
 import smartcar
@@ -7,10 +9,9 @@ from flask import Flask, redirect, request, jsonify, send_from_directory, sessio
 from flask_cors import CORS
 from twilio.rest import Client
 from twilio.twiml.messaging_response import MessagingResponse
-from config import *
-import json
+from pymongo import MongoClient
 
-import requests
+from config import *
 
 app = Flask(__name__, static_folder="client")
 app.secret_key = "not secret"
@@ -25,10 +26,7 @@ data_readings = {}
 # list of phone numbers of potential victims
 victims = []
 
-# stores email, password, phone number, and uid's of registered users
-users = {}
-
-client = smartcar.AuthClient(
+smartcar_client = smartcar.AuthClient(
     client_id=CLIENT_ID,
     client_secret=CLIENT_SECRET,
     redirect_uri=REDIRECT_URI,
@@ -37,6 +35,9 @@ client = smartcar.AuthClient(
 )
 
 twilio_client = Client(TWILIO_SID, TWILIO_AUTH_TOKEN)
+
+mongo_client = MongoClient(MONGO_URI)
+db = mongo_client.get_database()
 
 @app.route("/", methods=["GET"])
 def home():
@@ -48,22 +49,24 @@ def serve_file(path):
 
 @app.route("/register", methods=["GET"])
 def register():
-    global users
 
     email = request.args.get("email")
-    if email in users:
-        return "user with email already exists", 500
     psw = request.args.get("psw")
     psw_repeat = request.args.get("psw-repeat")
-    if psw != psw_repeat:
-        return "passwords do not match", 500
     phone = request.args.get("phone")
 
-    users[email] = {
+    user = db.users.find_one({"email": email})
+    if user:
+        return "user with email already exists", 500
+    if psw != psw_repeat:
+        return "passwords do not match", 500
+
+    db.users.insert_one({
+        "email": email,
         "psw": psw,
         "phone": phone,
         "uids": []
-    }
+    })
 
     session["email"] = email
     session["phone"] = phone
@@ -72,16 +75,16 @@ def register():
 
 @app.route("/login", methods=["GET"])
 def login():
-    global users
 
     email = request.args.get("email")
     psw = request.args.get("psw")
 
-    if email not in users or users[email].get("psw") != psw:
+    user = db.users.find_one({"email" : email})
+    if not user or user.get("psw") != psw:
         return "incorrect password"
     else:
         session["email"] = email
-        session["phone"] = users[email].get("phone")
+        session["phone"] = user.get("phone")
         return redirect("/")
 
 @app.route("/logout", methods=["GET"])
@@ -93,13 +96,16 @@ def logout():
 def whoami():
     if "email" not in session:
         return "not logged in"
+    user = db.users.find_one({"email": session.get("email")})
+    if not user:
+        return "not logged in"
     result = "<h1>Email</h1>"
     result += f"<p>{session.get('email')}</p>"
     result += "<h1>Phone number</h1>"
     result += f"<p>{session.get('phone')}</p>"
     result += "<h1>UIDs</h1>"
     result += "<ul>" 
-    for user_id in users[session.get('email')].get('uids'):
+    for user_id in user.get('uids'):
         result += f"<li>{user_id}</li>"
         token = access[user_id]["access_token"]
         vehicle_ids = smartcar.get_vehicle_ids(
@@ -114,7 +120,7 @@ def whoami():
 @app.route("/register_vehicle", methods=["GET"])
 def register_vehicle():
     if "email" in session:
-        auth_url = client.get_auth_url()
+        auth_url = smartcar_client.get_auth_url()
         return redirect(auth_url)
     else:
         return redirect("/signin.html")
@@ -123,33 +129,65 @@ def register_vehicle():
 def exchange():
     global access
 
+    email = session.get("email")
+    if not email:
+        return "not logged in"
+
     code = request.args.get("code")
 
-    user_access = client.exchange_code(code)
+    user_access = smartcar_client.exchange_code(code)
     user_id = smartcar.get_user_id(user_access["access_token"])
+    user_access["uid"] = user_id
+    db.access.insert_one(user_access)
     access[user_id] = user_access
 
-    users[session.get("email")].get("uids").append(user_id)
+    user = db.users.find_one({"email": email})
+    uids = user.get("uids")
+    uids.append(user_id)
+    db.users.update_one({"email": email}, {"$set": {"uids" : uids}})
     return "", 200
 
 
 @app.route("/vehicles", methods=["GET"])
 def vehicles():
     global access
+    
+    if "email" not in session:
+        return "not logged in"
+    email = session.get("email")
+    user = db.users.find_one({"email" : email})
+    uids = user.get("uids")
 
-    result = "<ul>"
-    for user_id in access:
-        result += f"<li>user: {user_id}</li>"
+    vehicles = []
+
+    for user_id in uids:
         token = access[user_id]["access_token"]
         vehicle_ids = smartcar.get_vehicle_ids(
             token)["vehicles"]
     
-        result += "<ul>"
         for vehicle in [smartcar.Vehicle(vehicle_id, token) for vehicle_id in vehicle_ids]:
-            result += f"<li>{vehicle.info()}</li>"
-        result += "</ul>"
-    result += "</ul>"
-    return result
+            vehicle_info = vehicle.info()
+            print(vehicle_info)
+            vehicles.append(f"{vehicle_info['make']} {vehicle_info['model']}")
+    return vehicles
+
+#@app.route("/vehicles", methods=["GET"])
+#def vehicles():
+#    global access
+#
+#    result = "<ul>"
+#    for user_id in access:
+#        result += f"<li>user: {user_id}</li>"
+#        token = access[user_id]["access_token"]
+#        vehicle_ids = smartcar.get_vehicle_ids(
+#            token)["vehicles"]
+#    
+#        result += "<ul>"
+#        for vehicle in [smartcar.Vehicle(vehicle_id, token) for vehicle_id in vehicle_ids]:
+#            result += f"<li>{vehicle.info()}</li>"
+#        result += "</ul>"
+#    result += "</ul>"
+#    return result
 
 
 @app.route("/data", methods=["GET"])
